b17298a236c61bf12927ada9a743d01c792ed3a1
nagykrisztian <krisznagy2001@gmail.com>
2020-10-22 23:38:25 +0200

    fej-vagy-iras

---
+++ src/Content.ts
@@ -0,0 +1,67 @@
+﻿import fs from "fs";
+import http from "http";
+import url from "url";
+import Megoldas from "./Megoldas";
+
+export default class Content {
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+        // favicon.ico kérés kiszolgálása:
+        if (req.url === "/favicon.ico") {
+            res.writeHead(200, { "Content-Type": "image/x-icon" });
+            fs.createReadStream("favicon.ico").pipe(res);
+            return;
+        }
+        // Weboldal inicializálása + head rész:
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write("<html lang='hu'>");
+        res.write("<head>");
+        res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
+        res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+        res.write("<title>Jedlik Ts Template</title>");
+        res.write("</head>");
+        res.write("<body><form><pre class='m-3'>");
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const params = url.parse(req.url as string, true).query;
+
+        // Kezd a kódolást innen -->
+        const megold = new Megoldas();
+
+        res.write("1. feladat\n");
+        res.write(`A pénzfeldobás eredménye: ${megold.penzFeldobas} \n`);
+
+        res.write("2. feladat\n");
+        const tipp: string = params.tipp as string;
+        res.write(`Tippeljen! (F/I)= <input type='text' value=' ' name='tipp' value=${tipp} style='max-width:100px;' onChange='this.form.submit();'>\n`);
+        if (tipp != null) {
+            if (megold.penzFeldobas == tipp) {
+                res.write("Ön eltalálta!\n");
+            } else {
+                res.write("Ön nem találta el.\n");
+            }
+        }
+
+        res.write("3. feladat\n");
+        res.write(`A kísérlet ${megold.dobasok.length.toString()} dobásból állt\n`);
+
+        res.write("4. feladat\n");
+        res.write(`A kísérlet során a fej relatív gyakorisága ${megold.fejGyakorisag}% volt.\n`);
+
+        res.write("5. feladat\n");
+        res.write(`A kísérlet során ${megold.egymasUtaniKetFej} alkalommal dobtak pontosan 2 fejet egymás után.\n`);
+
+        res.write("6. feladat\n");
+        res.write(`A leghosszabb tisztafej sorozat ${megold.fejSorozat[0]} tagból áll, kezdete a(z) ${megold.fejSorozat[1]}. dobás.\n`);
+
+        megold.ezerDobas();
+
+        res.write("<a href='https://github.com/nagykrisztian'>Git</a>");
+        res.write("<a href='https://github.com/nagykrisztian'>Heroku</a>");
+
+        // <---- Fejezd be a kódolást
+
+        res.write("</pre></form>");
+        res.write("</body></html>");
+        res.end();
+    }
+}
---
+++ src/Megoldas.ts
@@ -0,0 +1,99 @@
+import fs from "fs";
+
+export default class Megoldas {
+    private _dobasok: string[] = [];
+
+    public get dobasok(): string[] {
+        return this._dobasok;
+    }
+
+    public kiIr(): void {
+        this._dobasok.forEach(item => {
+            return item;
+        });
+    }
+
+    public get penzFeldobas(): string {
+        const random = +Math.random().toFixed(0);
+        // console.log(random);
+        let dobas: string;
+        if (random == 0) {
+            dobas = "I";
+        } else {
+            dobas = "F";
+        }
+        return dobas;
+    }
+
+    public get fejGyakorisag(): number {
+        let cnt: number = 0;
+        this._dobasok.forEach(i => {
+            if (i == "F") {
+                cnt++;
+            }
+        });
+        // console.log(cnt);
+        // console.log(this._dobasok.length);
+        return +((cnt / this._dobasok.length) * 100).toFixed(2);
+    }
+
+    public get egymasUtaniKetFej(): number {
+        let egymasUtaniF: number = 0;
+        for (let i = 1; i < this._dobasok.length - 3; i++) {
+            if (this._dobasok[i] == "F" && this._dobasok[i + 1] == "F" && this._dobasok[i + 2] != "F" && this._dobasok[i - 1] != "F") {
+                egymasUtaniF++;
+            }
+        }
+        return egymasUtaniF;
+    }
+
+    public get fejSorozat(): number[] {
+        let fDarab: number = 0;
+        let kezdoIndex: number = 0;
+        let legtobbF: number = 0;
+        for (let i = 0; i < this._dobasok.length; i++) {
+            if (this._dobasok[i] == "F") {
+                fDarab++;
+                if (legtobbF < fDarab) {
+                    legtobbF = fDarab;
+                    kezdoIndex = i - fDarab + 2;
+                }
+            } else {
+                fDarab = 0;
+            }
+        }
+
+        return [legtobbF, kezdoIndex];
+    }
+
+    public ezerDobas(): void {
+        const t: string[] = [];
+        let ffffDB: number = 0;
+        let fffiDB: number = 0;
+        for (let i = 0; i < 1000; i++) {
+            t.push(this.penzFeldobas + this.penzFeldobas + this.penzFeldobas + this.penzFeldobas);
+            console.log(t[i]);
+        }
+        t.forEach(x => {
+            if (x == "FFFF") {
+                ffffDB++;
+            } else if (x == "FFFI") {
+                fffiDB++;
+            }
+        });
+        fs.writeFileSync("dobasok.txt", `FFFF: ${ffffDB}, FFFI: ${fffiDB}\r\n`);
+        fs.appendFileSync("dobasok.txt", t.join(" "));
+    }
+
+    constructor() {
+        fs.readFileSync("kiserlet.txt")
+            .toString()
+            .split("\n")
+            .forEach(i => {
+                const aktSor: string = i.trim();
+                if (aktSor != "") {
+                    this._dobasok.push(aktSor);
+                }
+            });
+    }
+}
