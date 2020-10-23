502b3c448b8be2061748b48fe240b7c2cf5a38f6
nagykrisztian <krisznagy2001@gmail.com>
2020-10-24 01:21:22 +0200

    unit tests

--- src/__tests__/fej-vagy-iras.test.ts
+++ src/__tests__/fej-vagy-iras.test.ts
@@ -1,17 +1,26 @@
 import Megoldas from "../Megoldas";
 
-test("feladat4", () => {
-    const megold = new Megoldas();
-    expect(megold.fejGyakorisag).toBe(51.03);
-});
+describe("Megoldás osztály unit tesztek", () => {
+    const instance: Megoldas = new Megoldas();
 
-test("feladat5", () => {
-    const megold = new Megoldas();
-    expect(megold.egymasUtaniKetFej).toBe(259);
-});
+    it("Megoldás osztálypéldány ellenőrzése", async () => {
+        expect(instance).toBeInstanceOf(Megoldas);
+    });
 
-test("feladat6", () => {
-    const megold = new Megoldas();
-    expect(megold.fejSorozat[0]).toBe(11);
-    expect(megold.fejSorozat[1]).toBe(947);
+    it("3. feladat: Dobások száma", async () => {
+        expect(instance.dobasokSzama).toBe(4321);
+    });
+
+    it("4. feladat: Fej dobás relatív gyakorisága", async () => {
+        expect(instance.fejGyakorisag).toBe(51.03);
+    });
+
+    it("5. feladat: Pontosan 2 fej egymás után", async () => {
+        expect(instance.egymasUtaniKetFej).toBe(259);
+    });
+
+    it("6. feladat: Leghosszabb fejekből álló részsorozat", async () => {
+        expect(instance.fejSorozat[0]).toBe(11);
+        expect(instance.fejSorozat[1]).toBe(947);
+    });
 });
